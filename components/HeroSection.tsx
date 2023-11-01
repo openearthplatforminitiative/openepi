4e2863bf09588ce406ad428d66511d654f7e477e
Henrik Aspenes Vedal <henrikvedal@hotmail.com>
2023-11-01 15:57:28 +0100

    Desktop version only needs to fix posts design now

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -6,15 +6,14 @@ import { Background } from "@/public/icons/Background";
 
 export function HeroSection() {
 	return (
-		<section className="flex flex-row pt-[258px] pb-[299px] px-8 bg-[#FBFDF8]">
-			<div className="flex flex-row container justify-center">
-				<div>
-					<Typography variant="h1" color="black">
+		<section className="flex flex-row pt-[258px] pb-[299px] px-8 bg-[#FBFDF8] justify-center">
+			<div className="flex flex-row justify-center items-center">
+				<div className="w-[38%]">
+					<Typography variant="h1" color="black" className="text-[45px]">
 						Open Earth Platform Initiative
 					</Typography>
 					<Typography
-						variant="lead"
-						className="mt-6 mb-10 w-full text-black/80 md:max-w-full md:pr-16  lg:max-w-2xl xl:pr-28"
+						className="mt-6 mb-10 text-black/80 md:pr-16 text-[36px] font-[400]"
 					>
 						An open, global digital innovation platform for climate and nature.
 					</Typography>
--- components/MissionSection.tsx
+++ components/MissionSection.tsx
@@ -9,23 +9,23 @@ export default function MissionSection() {
 		<section className="py-28 px-8 bg-[#D1E8D5]">
 			<div className="flex flex-row justify-center items-center">
 				<MissionLogo />
-				<div className="container ml-[49px]">
+				<div className=" ml-[49px] lg:w-[42%]">
 					<Typography
 						color="black"
-						className="h-fit mb-[16px] text-[16px] font-[400] text-[#0C1F14]"
+						className="mb-[16px] text-[16px] font-[400] text-[#0C1F14] w-fit"
 					>
 						Our mission
 					</Typography>
 					<Typography
 						variant="h1"
 						color="blue-gray"
-						className="mb-[16px] text-[32px] text-[#0C1F14] font-[600]"
+						className="mb-[16px] text-[32px] text-[#0C1F14] font-[600] w-fit"
 					>
 						Local Tech Innovation
 					</Typography>
 					<Typography
 						variant="lead"
-						className="text-[#0C1F14] lg:w-11/12 "
+						className="text-[#0C1F14] lg:w-[62%]"
 					>
 						The OpenEPI project mission is to support local innovation that is
 						fuelled by a global platform of open data, technology and AI.
