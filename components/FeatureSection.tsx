61f9399d92cdc8d8fe9f972113ce9d15258fb2ad
Henrik Aspenes Vedal <henrikvedal@hotmail.com>
2023-11-02 12:03:23 +0100

    Promoted posts are now always placed first

--- components/FeatureSection.tsx
+++ components/FeatureSection.tsx
@@ -12,7 +12,7 @@ export default function FeatureSection({ featured }: FeatureSectionProps) {
 	return (
 		<section className="py-28 px-4 w-full bg-[#FBFDF8]">
 			<div className="container mx-auto grid max-w-6xl grid-cols-1 gap-[24px] md:grid-cols-2 ">
-				{featured.map(({ _id, title, description, buttons, promoted }) => (
+				{featured.sort((a) => a.promoted  ? -1 : 1).map(({ _id, title, description, buttons, promoted }) => (
 					<FeatureCard
 						key={_id}
 						title={title}
