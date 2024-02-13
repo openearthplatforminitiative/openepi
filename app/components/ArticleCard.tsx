796840d2840d8fc95d6295059ab4315939e01584
Erik Dymbe <erik.dymbe@gmail.com>
2024-02-13 17:57:18 +0100

    fix: align line height to height of arrow

--- app/components/ArticleCard.tsx
+++ app/components/ArticleCard.tsx
@@ -30,11 +30,8 @@ const ResourceCard = ({ header, imageUrl, href, alt }: ResourceCardProps) => {
 					className={"h-[240px] w-full object-cover"}
 				/>
 				<Box className={"flex flex-col p-6"}>
-					<Box className={"flex flex-row justify-between items-baseline"}>
-						<Typography
-							variant="h5"
-							className={"leading-9 text-2xl xs:text-3xl"}
-						>
+					<Box className={"flex flex-row justify-between items-start"}>
+						<Typography className={"leading-6 text-xl"}>
 							{header}
 						</Typography>
 						<Box className="transform transition-transform duration-300 group-hover:translate-x-2">
