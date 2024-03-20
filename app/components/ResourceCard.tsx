46554672cfcb4cef562447a512eb658883dc1cd9
Erik Dymbe <erik.dymbe@gmail.com>
2024-03-20 15:55:41 +0100

    fix: style ResourceCard according to spec

--- app/components/ResourceCard.tsx
+++ app/components/ResourceCard.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { Box, Card, Typography } from "@mui/material";
+import { Box, Card } from "@mui/material";
 import { InternalLinkIcon } from "@/app/icons/InternalLinkIcon";
 import { ExternalLinkIcon } from "@/app/icons/ExternalLinkIcon";
 
@@ -18,20 +18,16 @@ const ResourceCard = ({ href, text, externalLink }: ResourceCardProps) => {
 				"flex flex-1 lg:min-w-[400px] md:min-w-[340px] 2xs:min-w-[270px] 2xs:max-w-[calc(100%-0.75rem)] xs:max-w-[calc(50%-0.75rem)] xl:max-w-[calc(40%-0.75rem)]"
 			}
 		>
-			<Card className={"group shadow-none bg-card hover:bg-secondary-90 h-full w-full"}>
-				<Box className={"flex flex-col p-6 gap-3"}>
-					<Box className={"flex flex-row justify-between items-baseline"}>
-						<Typography variant="h5" className={"text-xl xs:text-2xl"}>
-							{text}
-						</Typography>
-						<Box className="transform transition-transform duration-300 group-hover:translate-x-2">
-							{externalLink ? (
-								<ExternalLinkIcon color={"black"} />
-							) : (
-								<InternalLinkIcon />
-							)}
-						</Box>
-					</Box>
+			<Card className={"group shadow-none rounded-xl bg-card p-6 gap-3 flex justify-between items-baseline hover:bg-secondary-90 h-full w-full"}>
+				<Box className={"text-xl xs:text-2xl"}>
+					{text}
+				</Box>
+				<Box className="transform transition-transform duration-300 group-hover:translate-x-2">
+					{externalLink ? (
+						<ExternalLinkIcon color={"black"} />
+					) : (
+						<InternalLinkIcon />
+					)}
 				</Box>
 			</Card>
 		</Link>
