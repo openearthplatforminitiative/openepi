22fafba9e8ce4258b1d06b2908aa9fc6cc41af26
Erik Dymbe <erik.dymbe@gmail.com>
2024-03-20 15:25:29 +0100

    fix: remove box shadows from cards

--- app/components/ResourceCard.tsx
+++ app/components/ResourceCard.tsx
@@ -18,7 +18,7 @@ const ResourceCard = ({ href, text, externalLink }: ResourceCardProps) => {
 				"flex flex-1 lg:min-w-[400px] md:min-w-[340px] 2xs:min-w-[270px] 2xs:max-w-[calc(100%-0.75rem)] xs:max-w-[calc(50%-0.75rem)] xl:max-w-[calc(40%-0.75rem)]"
 			}
 		>
-			<Card className={"group hover:bg-secondary-90 h-full w-full"}>
+			<Card className={"group shadow-none bg-card hover:bg-secondary-90 h-full w-full"}>
 				<Box className={"flex flex-col p-6 gap-3"}>
 					<Box className={"flex flex-row justify-between items-baseline"}>
 						<Typography variant="h5" className={"text-xl xs:text-2xl"}>
