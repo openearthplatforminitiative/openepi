12ee1776cb2224e4e9e98178ad71c51913994931
Erik Dymbe <erik.dymbe@gmail.com>
2024-03-20 17:40:27 +0100

    fix: style footer more consistently

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,5 +1,5 @@
 import { Partner } from "@/sanity/api";
-import { Box, Divider, List, ListItem, Typography } from "@mui/material";
+import { Box, Divider, Typography } from "@mui/material";
 import Link from "next/link";
 import { OpenEPILogoSmall } from "@/app/icons/OpenEPILogoSmall";
 import { ExternalLinkIconSmall } from "@/app/icons/ExternalLinkIconSmall";
@@ -10,89 +10,71 @@ interface FooterProps {
 
 export default function Footer({ partners }: FooterProps) {
 	return (
-		<footer className={"flex w-full flex-col items-center"}>
-			<Box className={"lg:max-w-7xl w-full"}>
-				<Box className={"flex  flex-col items-center p-6"}>
-					<Box className="flex flex-row gap-5 mb-6 mt-11 w-full">
-						<OpenEPILogoSmall />
-					</Box>
-					<Box
-						className={
-							"flex flex-wrap gap-x-[420px] lg:text-base text-lg w-full"
-						}
-					>
-						<Box>
-							<List className={"gap-4"}>
-								<ListItem>
-									<Link
-										className={"underline hover:no-underline"}
-										href={"/about"}
-									>
-										About OpenEPI
-									</Link>
-								</ListItem>
-								<ListItem>
-									<Link
-										className={"underline hover:no-underline"}
-										href={"/get-involved"}
-									>
-										Get involved
-									</Link>
-								</ListItem>
-								<ListItem>
-									<Link
-										className={"underline hover:no-underline"}
-										href={"/resources"}
-									>
-										Resources
-									</Link>
-								</ListItem>
-								<ListItem>
-									<a
-										target={"_blank"}
-										className={
-											"flex flex-row gap-1 items-center underline hover:no-underline"
-										}
-										href={"https://developer-test.openepi.io/"}
-									>
-										For developers <ExternalLinkIconSmall />
-									</a>
-								</ListItem>
-							</List>
-						</Box>
-						<Box>
-							<List className={"gap-4 "}>
-								<ListItem className={"text-secondary-main"}>
-									The OpenEPI project partners
-								</ListItem>
-								{partners.map((partner) => (
-									<ListItem key={partner.url}>
-										<Link
-											href={partner.url}
-											target={"_blank"}
-											className={
-												"flex flex-row gap-1 underline hover:no-underline items-center w-fit"
-											}
-										>
-											<Typography className="py-1 p-1 text-base font-400 transition-colors underline w-fit hover:no-underline">
-												{partner.title}
-											</Typography>
-											<ExternalLinkIconSmall />
-										</Link>
-									</ListItem>
-								))}
-							</List>
-						</Box>
-					</Box>
-					<Box className={"flex flex-col gap-6 pt-14 pb-20"}>
-						<Divider />
-						<Typography>
-							2024 - Except where otherwise noted, content on this site is
-							licensed under a Creative Commons Attribution 4.0 International
-							license.
-						</Typography>
-					</Box>
+		<footer className={"flex justify-center w-full pt-20 pb-28 px-6 sm:px-20"}>
+			<Box className={"flex flex-col max-w-6xl w-full"}>
+				<OpenEPILogoSmall />
+				<Box className={"flex flex-col sm:flex-row flex-wrap text-base w-full my-12 gap-y-12"}>
+					<ul className={"flex flex-1 flex-col gap-4"}>
+						<li>
+							<Link
+								className={"underline hover:no-underline"}
+								href={"/about"}
+							>
+								About OpenEPI
+							</Link>
+						</li>
+						<li>
+							<Link
+								className={"underline hover:no-underline"}
+								href={"/get-involved"}
+							>
+								Get involved
+							</Link>
+						</li>
+						<li>
+							<Link
+								className={"underline hover:no-underline"}
+								href={"/resources"}
+							>
+								Resources
+							</Link>
+						</li>
+						<li>
+							<Link
+								target={"_blank"}
+								className={
+									"flex gap-1 w-fit underline hover:no-underline items-center"
+								}
+								href={"https://developer-test.openepi.io/"}
+							>
+								For developers <ExternalLinkIconSmall />
+							</Link>
+						</li>
+					</ul>
+					<ul className={"flex flex-1 flex-col gap-4"}>
+						<li className={"text-secondary-main"}>
+							The OpenEPI project partners
+						</li>
+						{partners.map((partner) => (
+							<li key={partner.url}>
+								<Link
+									href={partner.url}
+									target={"_blank"}
+									className={"flex gap-1 w-fit underline hover:no-underline items-center"}
+								>
+									{partner.title}
+									<ExternalLinkIconSmall />
+								</Link>
+							</li>
+						))}
+					</ul>
 				</Box>
+				<Divider />
+				<Typography className={"mt-6"}>
+					2024 - Except where otherwise noted, content on this site is
+					licensed under a Creative Commons Attribution 4.0 International
+					license.
+				</Typography>
 			</Box>
 		</footer>
 	);
