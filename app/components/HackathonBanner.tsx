c0e022c39809d34638e84b2cea8c5e11d8c39bbe
Sander Krøglid <sander.kroglid@knowit.no>
2025-06-10 14:07:09 +0200

    update HackathonBanner with improved layout and button text

--- app/components/HackathonBanner.tsx
+++ app/components/HackathonBanner.tsx
@@ -2,23 +2,23 @@ import { Typography, Button } from "@mui/material"
 import Link from "next/link"
 
 export const HackathonBanner = () => (
-	<div className="sticky top-0 left-0 w-full bg-secondary-90 py-4 z-50">
-		<div className="max-w-6xl mx-auto flex gap-6 justify-center items-center">
-			<Typography className="text-xl font-bold">
-				Join our Open Data Hackathon 2025
-			</Typography>
-			<Link
-				href="https://developer.openepi.io/hackathon-2025"
-				target="_blank"
-				className="text-primary-main font-semibold ml-2"
-			>
-				<Button
-					variant="contained"
-					className="bg-primary-main rounded-full border-neutralVariant-50 normal-case shadow-none px-8 py-2"
-				>
-					Register now
-				</Button>
-			</Link>
-		</div>
-	</div>
+  <div className="sticky top-0 left-0 w-full bg-secondary-90 py-4 px-8 z-50">
+    <div className="max-w-6xl mx-auto flex gap-2 justify-center flex-wrap items-center">
+      <Typography className="text-xl font-bold">
+        Join our Open Data Hackathon 2025
+      </Typography>
+      <Link
+        href="https://developer.openepi.io/hackathon-2025"
+        target="_blank"
+        className="text-primary-main font-semibold ml-2"
+      >
+        <Button
+          variant="contained"
+          className="bg-primary-main rounded-full border-neutralVariant-50 normal-case shadow-none px-8 py-2"
+        >
+          Read More and Register now
+        </Button>
+      </Link>
+    </div>
+  </div>
 )
