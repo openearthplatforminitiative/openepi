d52f0a083c0097465450791d3c37d80d0e799a6e
Sander Krøglid <sander.kroglid@knowit.no>
2025-06-24 12:11:45 +0200

    update hackathon banner

--- app/components/HackathonBanner.tsx
+++ app/components/HackathonBanner.tsx
@@ -5,7 +5,7 @@ export const HackathonBanner = () => (
 	<div className="w-full bg-secondary-90 py-4 px-8 z-50">
 		<div className="max-w-6xl mx-auto flex gap-2 justify-center flex-wrap items-center">
 			<Typography className="text-xl font-bold">
-				Join our Open Data Hackathon 2025
+				Our Open Data Hackathon 2025 is now live
 			</Typography>
 			<Link
 				href="https://developer.openepi.io/hackathon-2025"
@@ -16,7 +16,7 @@ export const HackathonBanner = () => (
 					variant="contained"
 					className="bg-primary-main rounded-full border-neutralVariant-50 normal-case shadow-none px-8 py-2"
 				>
-					Read More and Register now
+					Read More
 				</Button>
 			</Link>
 		</div>
